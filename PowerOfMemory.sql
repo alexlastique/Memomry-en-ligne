25ed705736c64687700f0e76a0d36e3c6466d0a2
Alexis Godet <alexis.godet05@gmail.com>
2023-10-25 14:52:03 +0200

    corection

--- PowerOfMemory.sql
+++ PowerOfMemory.sql
@@ -323,15 +323,15 @@ SELECT
     WHERE S2.IdUser = PM.IdUser2
   ) AS NbPartieJouer2,
   (SELECT G.GameName FROM Score AS S
-LEFT JOIN Game AS G ON S.IdGame = G.Id
-ORDER BY 
- (SELECT COUNT(DISTINCT S.DateGame) AS NbParties FROM Score AS S) DESC
-LIMIT 1) AS JeuLePlusJoué1,
+    LEFT JOIN Game AS G ON S.IdGame = G.Id
+    ORDER BY 
+      (SELECT COUNT(DISTINCT S.DateGame) AS NbParties FROM Score AS S) DESC
+      LIMIT 1) AS JeuLePlusJoué1,
   (SELECT G.GameName FROM Score AS S
-LEFT JOIN Game AS G ON S.IdGame = G.Id
-ORDER BY 
- (SELECT COUNT(DISTINCT S.DateGame) AS NbParties FROM Score AS S) DESC
-LIMIT 1) AS JeuLePlusJoué2
+    LEFT JOIN Game AS G ON S.IdGame = G.Id
+    ORDER BY 
+      (SELECT COUNT(DISTINCT S.DateGame) AS NbParties FROM Score AS S) DESC
+      LIMIT 1) AS JeuLePlusJoué2
 FROM PrivedMessage AS PM
 LEFT JOIN Utilisateur AS U1 ON PM.IdUser1=U1.Id
 LEFT JOIN Utilisateur AS U2 ON PM.IdUser2=U2.Id
@@ -347,20 +347,20 @@ ORDER BY -PublichDate
 
 SELECT '2023' AS Année, m.mois AS Mois,
 (SELECT U.Pseudo FROM Score AS S
-LEFT JOIN Utilisateur AS U ON S.IdPlayer = U.Id
+LEFT JOIN Utilisateur AS U ON S.IdUser = U.Id
 WHERE MONTH(S.DateGame)=mois
 ORDER BY S.GameScore DESC
 LIMIT 1)
 AS 'Top 1',
 (SELECT U.Pseudo FROM Score AS S
-LEFT JOIN Utilisateur AS U ON S.IdPlayer = U.Id
+LEFT JOIN Utilisateur AS U ON S.IdUser = U.Id
 WHERE MONTH(S.DateGame)=mois
  AND U.Pseudo != `TOP 1`
 ORDER BY S.GameScore DESC
 LIMIT 1 OFFSET 1)
 AS 'Top 2',
 (SELECT U.Pseudo FROM Score AS S
-LEFT JOIN Utilisateur AS U ON S.IdPlayer = U.Id
+LEFT JOIN Utilisateur AS U ON S.IdUser = U.Id
 WHERE MONTH(S.DateGame)=mois
  AND U.Pseudo != `TOP 1`
  AND U.Pseudo != `TOP 2`
@@ -369,7 +369,7 @@ LIMIT 1 OFFSET 2)
 AS 'Top 3',
 (SELECT COUNT(DISTINCT S.DateGame) AS Total_parties FROM Score AS S
 WHERE MONTH(S.DateGame)=mois) AS 'Total parties',
-(SELECT G.Game FROM Score AS S
+(SELECT G.GameName FROM Score AS S
 LEFT JOIN Game AS G ON S.IdGame = G.Id
 WHERE MONTH(S.DateGame)=mois
 ORDER BY 
@@ -400,7 +400,7 @@ ORDER BY mois;
 SELECT '2023' AS Année, m.mois AS Mois,
 (SELECT COUNT(DISTINCT S.DateGame) AS Total_parties FROM Score AS S
 WHERE MONTH(S.DateGame)=mois) AS 'Total parties',
-(SELECT G.Game FROM Score AS S
+(SELECT G.GameName FROM Score AS S
 LEFT JOIN Game AS G ON S.IdGame = G.Id
 WHERE MONTH(S.DateGame)=mois
 ORDER BY 
