776bd8211f6e1aadef6f495ca43cde79803c5909
Mathias.G <mathiasghanem05@gmail.com>
2023-10-30 14:59:28 +0100

    database

--- utils/database.php
+++ utils/database.php
@@ -1,17 +1,23 @@
 <?php
-function connectToDbAndGetPdo($dsn, $user, $pass, $driver_options) {
-    $pdo = new PDO($dsn, $user, $pass, $driver_options);
-    return $pdo;
+function connectToDbAndGetPdo() {
+    $dbname = 'SpaceMemory';
+    $host = 'localhost';
+
+    $dsn = "mysql:dbname=$dbname;host=$host;charset=utf8";
+    $user = 'root';
+    $pass = 'root';
+
+    $driver_options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+    ];
+
+    try {
+        $pdo = new PDO($dsn, $user, $pass, $driver_options);
+        echo 'La connexion à la base de données est OK';
+
+        return $pdo;
+    } catch (PDOException $e) {
+        echo 'La connexion à la base de données a échouée.';
+    }
 }
-
-$dbname = 'SpaceMemory';
-$host = 'localhost';
-
-$dsn = "mysql:dbname=$dbname;host=$host;charset=utf8";
-$user = 'root';
-$pass = 'root';
-
-$driver_options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
-];
