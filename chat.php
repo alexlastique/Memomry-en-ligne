5afa36a76d23ecfaef742de6b2dde1f141d51b35
Gento91 <leo.kontente@gmail.com>
2023-10-30 14:28:30 +0100

    move chat dans chat.php

---
+++ chat.php
@@ -0,0 +1,37 @@
+<div id="pos_chat">
+    <input type="checkbox" id="toggle" class="toggle-checkbox">
+    <label for="toggle" class="floating-button"></label>
+    <div id="chat">
+        <div id="head">
+            <img src="../../assets/images/AvataBot.png"width="36px"height="36px">
+            <p>Chat générale</p>
+        </div>
+        <div id="messages">
+            <div class="flex">
+                <img src="../../assets/images/AvataBot.png"width="36px"height="36px">
+                <div class="column">
+                    <p>Quelqu'un</p>
+                    <div class="message">
+                        <p>Je sui qq</p>
+                    </div>
+                    <p>Aujourd'hui à 16h15</p>
+                </div>
+            </div>
+            <div class="flex">
+                <div class="column user">
+                    <p>Moi</p>
+                    <div class="message usersmessage">
+                        <p>Je suis moi</p>
+                    </div>
+                    <p>Aujourd'hui à 16h17</p>
+                </div>
+            </div>
+        </div>
+        <div id="input">
+            <form>
+                <input type="text" name="chat" placeholder="Votre message...">
+                <button><strong>Envoyer</strong></button>
+            </form>
+        </div>
+    </div>
+</div>
--- games/memory/index.php
+++ games/memory/index.php
@@ -17,45 +17,7 @@
                     <button id="difficile">Difficile</button>
                 </div>
             </article>
-
-            <div id="pos_chat">
-                <input type="checkbox" id="toggle" class="toggle-checkbox">
-                <label for="toggle" class="floating-button"></label>
-                <div id="chat">
-                    <div id="head">
-                        <img src="../../assets/images/AvataBot.png"width="36px"height="36px">
-                        <p>Chat générale</p>
-                    </div>
-                    <div id="messages">
-                        <div class="flex">
-                            <img src="../../assets/images/AvataBot.png"width="36px"height="36px">
-                            <div class="column">
-                                <p>Quelqu'un</p>
-                                <div class="message">
-                                    <p>Je sui qq</p>
-                                </div>
-                                <p>Aujourd'hui à 16h15</p>
-                            </div>
-                        </div>
-                        <div class="flex">
-                            <div class="column user">
-                                <p>Moi</p>
-                                <div class="message usersmessage">
-                                    <p>Je suis moi</p>
-                                </div>
-                                <p>Aujourd'hui à 16h17</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="input">
-                        <form>
-                            <input type="text" name="chat" placeholder="Votre message...">
-                            <button><strong>Envoyer</strong></button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            
+            <?php require_once SITE_ROOT.'chat.php';?>
             <table class="Scores" id="TableMemory1">
                 <tr>
                     <td><img src="../../assets/images/black-hole.avif" alt="trou noir"></td>
