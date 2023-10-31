22ebc9e4573da59d87235a937b87f63fbe3376a1
Alexis Godet <alexis.godet05@gmail.com>
2023-10-31 16:50:54 +0100

    register

--- games/memory/Score.php
+++ games/memory/Score.php
@@ -4,11 +4,18 @@
 require_once '../../utils/common.php';
 require_once SITE_ROOT . 'partials/head.php';
 require_once SITE_ROOT . 'utils/database.php';
+$PseudoShearch = '';
+if (isset($_POST['name'])) {
+    $PseudoShearch = $_POST['name'];
+}
+
 $pdo = connectToDbAndGetPdo();
-$pdoStatement = $pdo->prepare('SELECT Game.GameName, Utilisateur.Pseudo, GameDifficult, GameScore FROM Score
+$pdoStatement = $pdo->prepare("SELECT Game.GameName, Utilisateur.Pseudo, GameDifficult, GameScore, IdUser FROM Score
 LEFT JOIN Game ON Score.IdGame = Game.Id
 LEFT JOIN Utilisateur On Score.IdUser = Utilisateur.Id
-ORDER BY IdGame, GameDifficult, GameScore ASC');
+Where Utilisateur.Pseudo LIKE '%$PseudoShearch%'
+    OR '$PseudoShearch'=''
+ORDER BY IdGame, GameDifficult, GameScore ASC");
 $pdoStatement->execute();
 $scores = $pdoStatement->fetchAll();
 ?>
@@ -20,7 +27,13 @@ $scores = $pdoStatement->fetchAll();
         ?>
         <h2 class="connexion">Score</h2>
     </header>
-
+        <form method="post">
+            <div>
+                <input name="name" type="text" placeholder="Rechercher un Profil 🔎" class="searchProfil">
+            </div>    
+            <button>Envoyer</button>
+        </form>
+        
         <table class="PageScores">
                 <tr>
                     <th>Nom du Jeu</th>
@@ -30,13 +43,23 @@ $scores = $pdoStatement->fetchAll();
                 </tr>
                 <?php
                 foreach ($scores as $score) {
+                    // if($_SESSION['user_id']==$score->IdUser):
+                    if(1==$score->IdUser):
                     ?>
+                <tr style="color: orange;">
+                    <td><?php echo $score->GameName ?></td>
+                    <td><?php echo $score->Pseudo ?></td>
+                    <td><?php echo $score->GameDifficult == 1 ? "Facile" : ($score->GameDifficult == 2 ? "Moyen" : "Difficile")?></td>
+                    <td><?php echo $score->GameScore; ?></td>
+                </tr>
+                <?php else:?>
                 <tr>
                     <td><?php echo $score->GameName ?></td>
                     <td><?php echo $score->Pseudo ?></td>
                     <td><?php echo $score->GameDifficult == 1 ? "Facile" : ($score->GameDifficult == 2 ? "Moyen" : "Difficile")?></td>
                     <td><?php echo $score->GameScore; ?></td>
                 </tr>
+                <?php endif;?>
                 <?php 
                 }
                 ?>
--- register.php
+++ register.php
@@ -2,7 +2,38 @@
 <html>
         <?php
             require_once 'utils/common.php';
-            require_once SITE_ROOT.'partials/head.php'
+            require_once SITE_ROOT.'partials/head.php';
+            require_once SITE_ROOT . 'utils/database.php';
+            $passwordPattern = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$._%^&*]).{8,}$/'; 
+            $pseudoPattern = '/^.{4,}$/'; 
+
+
+            if (isset($_POST['RegisterEmail']) && isset($_POST['RegisterPseudo']) && isset($_POST['RegisterPassword']) && isset($_POST['RegisterPassword2'])) {
+                $RegisterEmail = $_POST['RegisterEmail'];
+                $RegisterPseudo = $_POST['RegisterPseudo'];
+                $RegisterPassword = $_POST['RegisterPassword'];
+                $RegisterPassword2 = $_POST['RegisterPassword2'];
+                if(preg_match($passwordPattern, $RegisterPassword) && preg_match($passwordPattern, $RegisterPassword2) && preg_match($pseudoPattern, $RegisterPseudo)){
+                    if (!filter_var($RegisterEmail, FILTER_VALIDATE_EMAIL)) {
+                        throw new Exception("Le format de l'email n'est pas valide");
+                    }
+                    else{
+                        if($RegisterPassword == $RegisterPassword2){
+                            $HashPassword = hash(
+                                'sha512',
+                                 $RegisterPassword,
+                            );
+                            $pdo = connectToDbAndGetPdo();
+                            $pdoStatement = $pdo->prepare("INSERT INTO Utilisateur (Email, PasswordUser, Pseudo)
+                            VALUES
+                                ('$RegisterEmail', '$HashPassword', '$RegisterPseudo')");
+                            $pdoStatement->execute();
+                            $scores = $pdoStatement->fetchAll();
+                        }
+                    }
+                }
+
+            }
         ?>
     <body>
         <header class="login header">
@@ -11,11 +42,11 @@
             ?>
             <h2 class="connexion">Inscription</h2>
         </header>
-        <form class="divInput">
-            <p class="pInput"><input type="email" placeholder="Email"></p>
-            <p class="pInput"><input type="pseudo" placeholder="Pseudo"></p>
-            <p class="pInput"><input type="password" placeholder="Mot de passe"></p>
-            <p class="pInput"><input type="password" placeholder="Confirmer le mot de passe"></p>
+        <form class="divInput" method="post">
+            <p class="pInput"><input name="RegisterEmail" type="email" placeholder="Email"></p>
+            <p class="pInput"><input name="RegisterPseudo" type="pseudo" placeholder="Pseudo"></p>
+            <p class="pInput"><input name="RegisterPassword" type="password" placeholder="Mot de passe"></p>
+            <p class="pInput"><input name="RegisterPassword2" type="password" placeholder="Confirmer le mot de passe"></p>
             <p class="pInput"><input type="submit" value="Inscription" class="Submit"></p>
             <p class="pInput2" >Déjà un compte ? Connecte-toi <a href="login.php" style="color: orange;">ici</a></p>
         </form>
