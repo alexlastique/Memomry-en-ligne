9ee72fa3781a00e7d7c0656157d9bb203964b4e1
Alexis Godet <alexis.godet05@gmail.com>
2023-11-01 15:31:54 +0100

    Score +nom dans le header

--- games/memory/Score.php
+++ games/memory/Score.php
@@ -1,74 +1,73 @@
 <!DOCTYPE html>
 <html>
-<?php
-require_once '../../utils/common.php';
-require_once SITE_ROOT . 'partials/head.php';
-require_once SITE_ROOT . 'utils/database.php';
-$PseudoShearch = '';
-if (isset($_POST['name'])) {
-    $PseudoShearch = $_POST['name'];
-}
-
-$pdo = connectToDbAndGetPdo();
-$pdoStatement = $pdo->prepare("SELECT Game.GameName, Utilisateur.Pseudo, GameDifficult, GameScore, IdUser FROM Score
-LEFT JOIN Game ON Score.IdGame = Game.Id
-LEFT JOIN Utilisateur On Score.IdUser = Utilisateur.Id
-Where Utilisateur.Pseudo LIKE '%$PseudoShearch%'
-    OR '$PseudoShearch'=''
-ORDER BY IdGame, GameDifficult, GameScore ASC");
-$pdoStatement->execute();
-$scores = $pdoStatement->fetchAll();
-?>
-
-<body>
-    <header class="login header">
-        <?php
-        require_once SITE_ROOT . 'partials/header.php';
-        ?>
-        <h2 class="connexion">Score</h2>
-    </header>
-        <form method="post">
-            <div>
-                <input name="name" type="text" placeholder="Rechercher un Profil 🔎" class="searchProfil">
-            </div>    
-            <button>Envoyer</button>
-        </form>
-        
-        <table class="PageScores">
-                <tr>
-                    <th>Nom du Jeu</th>
-                    <th>Pseudo du Joueur</th>
-                    <th>Score du Joueur</th>
-                    <th>Niveau de Difficulté</th>
-                </tr>
-                <?php
-                foreach ($scores as $score) {
-                    // if($_SESSION['user_id']==$score->IdUser):
-                    if(1==$score->IdUser):
-                    ?>
-                <tr style="color: orange;">
-                    <td><?php echo $score->GameName ?></td>
-                    <td><?php echo $score->Pseudo ?></td>
-                    <td><?php echo $score->GameDifficult == 1 ? "Facile" : ($score->GameDifficult == 2 ? "Moyen" : "Difficile")?></td>
-                    <td><?php echo $score->GameScore; ?></td>
-                </tr>
-                <?php else:?>
-                <tr>
-                    <td><?php echo $score->GameName ?></td>
-                    <td><?php echo $score->Pseudo ?></td>
-                    <td><?php echo $score->GameDifficult == 1 ? "Facile" : ($score->GameDifficult == 2 ? "Moyen" : "Difficile")?></td>
-                    <td><?php echo $score->GameScore; ?></td>
-                </tr>
-                <?php endif;?>
-                <?php 
-                }
-                ?>
-        </table>
-    <!--footer-->
     <?php
-    require_once SITE_ROOT . 'partials/footer.php';
+        require_once '../../utils/common.php';
+        require_once SITE_ROOT . 'partials/head.php';
+        require_once SITE_ROOT . 'utils/database.php';
+        $PseudoShearch = '';
+        $IdUser = $_SESSION['userId'];
+
+        if (isset($_POST['name'])) {
+            $PseudoShearch = $_POST['name'];
+        }
+
+        $pdo = connectToDbAndGetPdo();
+        $pdoStatement = $pdo->prepare("SELECT Game.GameName, Utilisateur.Pseudo, GameDifficult, GameScore, IdUser FROM Score
+        LEFT JOIN Game ON Score.IdGame = Game.Id
+        LEFT JOIN Utilisateur On Score.IdUser = Utilisateur.Id
+        Where Utilisateur.Pseudo LIKE '%$PseudoShearch%'
+            OR '$PseudoShearch'=''
+        ORDER BY IdGame, GameDifficult, GameScore ASC");
+        $pdoStatement->execute();
+        $scores = $pdoStatement->fetchAll();
     ?>
-    <!--fin footer-->
-</body>
+
+    <body>
+        <header class="login header">
+            <?php
+                require_once SITE_ROOT . 'partials/header.php';
+            ?>
+            <h2 class="connexion">Score</h2>
+        </header>
+            <form method="post">
+                <div>
+                    <input name="name" type="text" placeholder="Rechercher un Profil 🔎" class="searchProfil">
+                </div>    
+                <button>Envoyer</button>
+            </form>
+            
+            <table class="PageScores">
+                    <tr>
+                        <th>Nom du Jeu</th>
+                        <th>Pseudo du Joueur</th>
+                        <th>Score du Joueur</th>
+                        <th>Niveau de Difficulté</th>
+                    </tr>
+                    <?php
+                        foreach ($scores as $score) {
+                            if($IdUser==$score->IdUser):
+                    ?>
+                    <tr style="color: orange;">
+                        <td><?php echo $score->GameName ?></td>
+                        <td><?php echo $score->Pseudo ?></td>
+                        <td><?php echo $score->GameDifficult == 1 ? "Facile" : ($score->GameDifficult == 2 ? "Moyen" : "Difficile")?></td>
+                        <td><?php echo $score->GameScore; ?></td>
+                    </tr>
+                    <?php else:?>
+                    <tr>
+                        <td><?php echo $score->GameName ?></td>
+                        <td><?php echo $score->Pseudo ?></td>
+                        <td><?php echo $score->GameDifficult == 1 ? "Facile" : ($score->GameDifficult == 2 ? "Moyen" : "Difficile")?></td>
+                        <td><?php echo $score->GameScore; ?></td>
+                    </tr>
+                    <?php endif;?>
+                    <?php }?>
+            </table>
+            
+        <?php
+            require_once SITE_ROOT . 'partials/footer.php';
+        ?>
+        
+    </body>
 
 </html>
--- partials/header.php
+++ partials/header.php
@@ -1,60 +1,121 @@
-<?php
-$MaPage=($_SERVER['REQUEST_URI']);
-$IdUser = $_SESSION['userId'];
-$pdo = connectToDbAndGetPdo();
-$pdoStatement = $pdo->prepare("SELECT Pseudo FROM Utilisateur
-WHERE Id = '$IdUser';");
-$pdoStatement->execute();
-$PseudoUser = $pdoStatement->fetch();
-?>
+<?php if(!empty($_SESSION)):?>
+    <?php
+    $MaPage=($_SERVER['REQUEST_URI']);
+    $IdUser = $_SESSION['userId'];
+    $pdo = connectToDbAndGetPdo();
+    $pdoStatement = $pdo->prepare("SELECT Pseudo FROM Utilisateur
+    WHERE Id = '$IdUser';");
+    $pdoStatement->execute();
+    $PseudoUser = $pdoStatement->fetch();
+    ?>
 
-<?php if ($MaPage != "/Projet-Flash/index.php"): ?>
-    
-    <div id="backgroundHeader">
-                    <nav>
-                        <div id="logo" class="logo">Space Memory</div>
-                        <input class="menu-btn" type="checkbox" id="menu-btn" />
-                        <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
-                        <!-- <input type="text" placeholder="Rechercher un Profil 🔎" class="searchProfil"> -->
+    <?php if ($MaPage != "/Projet-Flash/index.php"): ?>
+        
+        <div id="backgroundHeader">
+                        <nav>
+                            <div id="logo" class="logo">Space Memory</div>
+                            <input class="menu-btn" type="checkbox" id="menu-btn" />
+                            <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+                            <!-- <input type="text" placeholder="Rechercher un Profil 🔎" class="searchProfil"> -->
+                            <ul class="menu">
+                                <li><a href="<?=PROJECT_FOLDER?>index.php" >Accueil</a></li>
+
+                                <li><a style="color: <?=$MaPage == "/Projet-Flash/games/memory/index.php" ? "orange" : "";?>;" href="<?=PROJECT_FOLDER?>games/memory/index.php" >Jeu</a></li>
+                                
+                                <li><a style="color: <?=$MaPage == "/Projet-Flash/games/memory/Score.php" ? "orange" : "";?>;" href="<?=PROJECT_FOLDER?>games/memory/Score.php" >Score</a></li>
+                                
+                                <li><a style="color: <?=$MaPage == "/Projet-Flash/contact.php" ? "orange" : "";?>;" href="<?=PROJECT_FOLDER?>contact.php">Nous Contacter</a></li>
+
+                                <li><a style="color: <?=$MaPage == "/Projet-Flash/myAccount.php" ? "orange" : "";?>;" href="#" id="LienConditionnel">Mon Espace(<?=$PseudoUser->Pseudo?>)</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <audio controls preload="auto" autoplay loop>
+                        <source src="<?=PROJECT_FOLDER?>assets/musique/<?=$MaPage == "/Projet-Flash/games/memory/index.php" ? "MusiqueAction.mp3" : "MusiqueTranquille.mp3";?>" type="audio/mpeg" />
+                        votre navigateur ne suporte pas l'element audio
+                    </audio>
+
+    <?php else: ?>
+
+        <nav>
+                    <div id="logo" class="logo">Space Memory</div>
+                    <input class="menu-btn" type="checkbox" id="menu-btn" />
+                    <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+                    <!-- <input type="text" placeholder="Rechercher un Profil 🔎" class="searchProfil"> -->
                         <ul class="menu">
-                            <li><a href="<?=PROJECT_FOLDER?>index.php" >Accueil</a></li>
-
-                            <li><a style="color: <?=$MaPage == "/Projet-Flash/games/memory/index.php" ? "orange" : "";?>;" href="<?=PROJECT_FOLDER?>games/memory/index.php" >Jeu</a></li>
-                            
-                            <li><a style="color: <?=$MaPage == "/Projet-Flash/games/memory/Score.php" ? "orange" : "";?>;" href="<?=PROJECT_FOLDER?>games/memory/Score.php" >Score</a></li>
-                            
-                            <li><a style="color: <?=$MaPage == "/Projet-Flash/contact.php" ? "orange" : "";?>;" href="<?=PROJECT_FOLDER?>contact.php">Nous Contacter</a></li>
-
-                            <li><a style="color: <?=$MaPage == "/Projet-Flash/myAccount.php" ? "orange" : "";?>;" href="#" id="LienConditionnel">Mon Espace(<?=$PseudoUser->Pseudo?>)</a></li>
+                            <li><a style="color: orange;" href="<?=PROJECT_FOLDER?>index.php" >Accueil</a></li>
+                            <li><a href="<?=PROJECT_FOLDER?>games/memory/index.php" >Jeu</a></li>
+                            <li><a href="<?=PROJECT_FOLDER?>games/memory/Score.php" >Score</a></li>
+                            <li><a href="<?=PROJECT_FOLDER?>contact.php">Nous Contacter</a></li>
+                            <li><a href="<?=PROJECT_FOLDER?>myAccount.php" id="LienConditionnel">Mon Espace(<?=$PseudoUser->Pseudo?>)</a></li>
                         </ul>
                     </nav>
-                </div>
-                <audio controls preload="auto" autoplay loop>
-                    <source src="<?=PROJECT_FOLDER?>assets/musique/<?=$MaPage == "/Projet-Flash/games/memory/index.php" ? "MusiqueAction.mp3" : "MusiqueTranquille.mp3";?>" type="audio/mpeg" />
-                    votre navigateur ne suporte pas l'element audio
-                </audio>
+                <h1>BIENVENUE DANS NOTRE STUDIO !</h1>
+            <p>Venez challenger les cerveaux les plus agiles !</p>
+            <a href="games/memory/index.php" id="playButton"><button>Jouer !</button></a>
+            <audio controls preload="auto" autoplay loop>
+                <source src="assets/musique/MusiqueTranquille.mp3" type="audio/mpeg" />
+                votre navigateur ne suporte pas l'element audio
+            </audio>
 
-<?php else: ?>
+    <?php endif; ?>
 
-    <nav>
-                <div id="logo" class="logo">Space Memory</div>
-                <input class="menu-btn" type="checkbox" id="menu-btn" />
-                <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
-                <!-- <input type="text" placeholder="Rechercher un Profil 🔎" class="searchProfil"> -->
-                    <ul class="menu">
-                        <li><a style="color: orange;" href="<?=PROJECT_FOLDER?>index.php" >Accueil</a></li>
-                        <li><a href="<?=PROJECT_FOLDER?>games/memory/index.php" >Jeu</a></li>
-                        <li><a href="<?=PROJECT_FOLDER?>games/memory/Score.php" >Score</a></li>
-                        <li><a href="<?=PROJECT_FOLDER?>contact.php">Nous Contacter</a></li>
-                        <li><a href="<?=PROJECT_FOLDER?>myAccount.php" id="LienConditionnel">Mon Espace(<?=$PseudoUser->Pseudo?>)</a></li>
-                    </ul>
-                </nav>
-            <h1>BIENVENUE DANS NOTRE STUDIO !</h1>
-        <p>Venez challenger les cerveaux les plus agiles !</p>
-        <a href="games/memory/index.php" id="playButton"><button>Jouer !</button></a>
-        <audio controls preload="auto" autoplay loop>
-            <source src="assets/musique/MusiqueTranquille.mp3" type="audio/mpeg" />
-             votre navigateur ne suporte pas l'element audio
-        </audio>
+<?php else:?>
+    <?php
+    $MaPage=($_SERVER['REQUEST_URI']);
+    ?>
+
+    <?php if ($MaPage != "/Projet-Flash/index.php"): ?>
+        
+        <div id="backgroundHeader">
+                        <nav>
+                            <div id="logo" class="logo">Space Memory</div>
+                            <input class="menu-btn" type="checkbox" id="menu-btn" />
+                            <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+                            <!-- <input type="text" placeholder="Rechercher un Profil 🔎" class="searchProfil"> -->
+                            <ul class="menu">
+                                <li><a href="<?=PROJECT_FOLDER?>index.php" >Accueil</a></li>
+
+                                <li><a style="color: <?=$MaPage == "/Projet-Flash/games/memory/index.php" ? "orange" : "";?>;" href="<?=PROJECT_FOLDER?>games/memory/index.php" >Jeu</a></li>
+                                
+                                <li><a style="color: <?=$MaPage == "/Projet-Flash/games/memory/Score.php" ? "orange" : "";?>;" href="<?=PROJECT_FOLDER?>games/memory/Score.php" >Score</a></li>
+                                
+                                <li><a style="color: <?=$MaPage == "/Projet-Flash/contact.php" ? "orange" : "";?>;" href="<?=PROJECT_FOLDER?>contact.php">Nous Contacter</a></li>
+
+                                <li><a style="color: <?=$MaPage == "/Projet-Flash/myAccount.php" ? "orange" : "";?>;" href="#" id="LienConditionnel">Mon Espace</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <audio controls preload="auto" autoplay loop>
+                        <source src="<?=PROJECT_FOLDER?>assets/musique/<?=$MaPage == "/Projet-Flash/games/memory/index.php" ? "MusiqueAction.mp3" : "MusiqueTranquille.mp3";?>" type="audio/mpeg" />
+                        votre navigateur ne suporte pas l'element audio
+                    </audio>
+
+    <?php else: ?>
+
+        <nav>
+                    <div id="logo" class="logo">Space Memory</div>
+                    <input class="menu-btn" type="checkbox" id="menu-btn" />
+                    <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+                    <!-- <input type="text" placeholder="Rechercher un Profil 🔎" class="searchProfil"> -->
+                        <ul class="menu">
+                            <li><a style="color: orange;" href="<?=PROJECT_FOLDER?>index.php" >Accueil</a></li>
+                            <li><a href="<?=PROJECT_FOLDER?>games/memory/index.php" >Jeu</a></li>
+                            <li><a href="<?=PROJECT_FOLDER?>games/memory/Score.php" >Score</a></li>
+                            <li><a href="<?=PROJECT_FOLDER?>contact.php">Nous Contacter</a></li>
+                            <li><a href="<?=PROJECT_FOLDER?>myAccount.php" id="LienConditionnel">Mon Espace</a></li>
+                        </ul>
+                    </nav>
+                <h1>BIENVENUE DANS NOTRE STUDIO !</h1>
+            <p>Venez challenger les cerveaux les plus agiles !</p>
+            <a href="games/memory/index.php" id="playButton"><button>Jouer !</button></a>
+            <audio controls preload="auto" autoplay loop>
+                <source src="assets/musique/MusiqueTranquille.mp3" type="audio/mpeg" />
+                votre navigateur ne suporte pas l'element audio
+            </audio>
+
+    <?php endif; ?>
+
+
 
 <?php endif; ?>
