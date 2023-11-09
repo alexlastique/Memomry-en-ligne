c6a0793166180939cd0cd825c37b85b05bcdfd9a
Mathias.G <mathiasghanem05@gmail.com>
2023-11-09 10:18:25 +0100

    img

--- partials/head.php
+++ partials/head.php
@@ -1,19 +1,9 @@
 <head>
     <title>Space memory</title>
     <meta charset="utf-8">
+    <link rel="icon" type="image/x-icon" href="<?=PROJECT_FOLDER?>assets/images/galaxie.png">
     <link rel="stylesheet" href="<?=PROJECT_FOLDER?>assets/css/normalize.css">
     <link rel="stylesheet" href="<?=PROJECT_FOLDER?>assets/css/footer.css">
     <link rel="stylesheet" href="<?=PROJECT_FOLDER?>assets/css/header.css">
     <link rel="stylesheet" href="<?=PROJECT_FOLDER?>assets/css/main.css">
 </head>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
