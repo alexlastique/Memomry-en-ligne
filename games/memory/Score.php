65d6b3cfcef9ae1a195d6898c1c92e2afcec2523
Mathias.G <mathiasghanem05@gmail.com>
2023-11-09 10:10:55 +0100

    lang

--- games/memory/Score.php
+++ games/memory/Score.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
     <?php
         require_once '../../utils/common.php';
         require_once SITE_ROOT . 'partials/head.php';
--- partials/head.php
+++ partials/head.php
@@ -6,3 +6,14 @@
     <link rel="stylesheet" href="<?=PROJECT_FOLDER?>assets/css/header.css">
     <link rel="stylesheet" href="<?=PROJECT_FOLDER?>assets/css/main.css">
 </head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
